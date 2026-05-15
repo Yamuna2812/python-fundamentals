77fdf44294d04ad9b20fe66bc41a206d749221a9
Yamuna <hyamuna2807@gmail.com>
2026-05-15 22:00:24 +0530

    Implement LRUCache class with get and put methods

---
+++ 9.py
@@ -0,0 +1,30 @@
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = {}
+        self.order = []
+        self.capacity = capacity
+
+    def get(self, key):
+        if key in self.cache:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.order.remove(key)
+        elif len(self.cache) >= self.capacity:
+            oldest = self.order.pop(0)
+            del self.cache[oldest]
+
+        self.cache[key] = value
+        self.order.append(key)
+
+
+cache = LRUCache(2)
+cache.put(1, 1)
+cache.put(2, 2)
+print(cache.get(1))
+cache.put(3, 3)
+print(cache.get(2))
