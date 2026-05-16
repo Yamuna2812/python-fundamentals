3372f73fb13d3a83a1436ce2670f1cc024bba0c6
Yamuna <hyamuna2807@gmail.com>
2026-05-16 21:49:16 +0530

    Fix print statement formatting in 27.py

---
+++ 27.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def max_sliding_window(nums, k):
+    dq = deque()
+    result = []
+
+    for i in range(len(nums)):
+        while dq and dq[0] <= i-k:
+            dq.popleft()
+
+        while dq and nums[dq[-1]] < nums[i]:
+            dq.pop()
+
+        dq.append(i)
+
+        if i >= k-1:
+            result.append(nums[dq[0]])
+
+    return result
+
+
+nums = [1,3,-1,-3,5,3,6,7]
+k = 3
+
+print(max_sliding_window(nums, k))
