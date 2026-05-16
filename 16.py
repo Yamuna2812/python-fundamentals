88e4d0869ef5658065171110001e66f9635b0e22
Yamuna <hyamuna2807@gmail.com>
2026-05-16 21:42:56 +0530

    Implement topological sorting algorithm

---
+++ 16.py
@@ -0,0 +1,30 @@
+def topo_sort(graph):
+    visited = set()
+    stack = []
+
+    def dfs(node):
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs(neighbor)
+        stack.append(node)
+
+    for node in graph:
+        if node not in visited:
+            dfs(node)
+
+    return stack[::-1]
+
+
+graph = {
+    'A': ['C'],
+    'B': ['C', 'D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': ['H', 'F'],
+    'F': ['G'],
+    'G': [],
+    'H': []
+}
+
+print("Topological order:", topo_sort(graph))
